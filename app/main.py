361c0395ae48ffe54f06b7e29937004effcea712
IT CPI <syariffortask@gmail.com>
2025-10-20 17:41:52 +0700

    update CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import item
 from app.api import transaction
 from app.api import user
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(item.router)
